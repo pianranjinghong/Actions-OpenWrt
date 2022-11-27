12edcec70a830f98bafd2e48d80edcfc7c7969ed
pianranjinghong <806481345@qq.com>
2022-11-27 23:08:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -30,6 +30,8 @@ git clone -b packages https://github.com/xiaorouji/openwrt-passwall package/xiao
 #git clone https://github.com/jerrykuku/luci-app-vssr package/jerrykuku/luci-app-vssr
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/xiaorouji/openwrt-passwall-luci
 svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-netspeedtest package/kiddin9/luci-app-netspeedtest
+git clone https://github.com/ZeaKyX/speedtest-web package/ZeaKyX/speedtest-web
+git clone https://github.com/sivel/speedtest-cli.git package/sivel/speedtest-cli
 git clone https://github.com/sirpdboy/luci-app-netdata feeds/luci/applications/luci-app-netdata
 #svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-netdata  feeds/luci/applications/luci-app-netdata
 #svn export --force https://github.com/kiddin9/openwrt-packages/trunk/netdata feeds/packages/admin/netdata
