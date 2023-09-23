980b1b5451a6e49ebe2107bfebe21f5986201e28
pianranjinghong <806481345@qq.com>
2023-09-23 16:55:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,12 +20,8 @@
 #rm -rf feeds/luci/themes/luci-theme-argon    # 删除自带argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon    # 替换新版argon
 #git clone https://github.com/QiuSimons/openwrt-mos package/QiuSimons/openwrt-mosdns
-#git clone https://github.com/sbwml/luci-app-mosdns package/sbwml/luci-app-mosdns
-#git clone https://github.com/sbwml/v2ray-geodata package/sbwml/luci-app-mosdns/v2ray-geodata
 #git clone https://github.com/xiaorouji/openwrt-passwall2 package/xiaorouji/openwrt-passwall2
-#git clone -b packages https://github.com/xiaorouji/openwrt-passwall package/xiaorouji/openwrt-passwall
 #git clone https://github.com/jerrykuku/luci-app-vssr package/jerrykuku/luci-app-vssr
-#git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/xiaorouji/openwrt-passwall-luci
 #svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-netspeedtest package/kiddin9/luci-app-netspeedtest
 #git clone https://github.com/ZeaKyX/speedtest-web package/ZeaKyX/speedtest-web
 #git clone https://github.com/sivel/speedtest-cli.git package/sivel/speedtest-cli
@@ -34,7 +30,6 @@
 
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-#rm -rf package/xiaorouji/openwrt-passwall/v2ray-geodata   # 删除v2ray-geodata
 rm -rf feeds/packages/net/mosdns
 #rm -rf feeds/packages/net/v2ray-geodata
 
@@ -42,10 +37,10 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sundaqiang/openwrt-packages package/sundaqiang
 svn export --force https://github.com/sundaqiang/openwrt-packages-backup/branches/main/luci/applications/luci-app-rebootschedule package/sundaqiang/luci-app-rebootschedule
-#git clone https://github.com/sbwml/v2ray-geodata feeds/packages/net/v2ray-geodata
+git clone -b v5 https://github.com/sbwml/luci-app-mosdns package/sbwml/luci-app-mosdns
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/pymumu/luci-app-smartdns
 #git clone https://github.com/xiaorouji/openwrt-passwall2 package/xiaorouji/openwrt-passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/xiaorouji/openwrt-passwall-packages
 git clone https://github.com/xiaorouji/openwrt-passwall package/xiaorouji/openwrt-passwall
 rm -rf package/xiaorouji/openwrt-passwall-packages/v2ray-geodata   # 删除v2ray-geodata
-git clone https://github.com/QiuSimons/openwrt-mos package/QiuSimons/openwrt-mos 
+#git clone https://github.com/QiuSimons/openwrt-mos package/QiuSimons/openwrt-mos 
